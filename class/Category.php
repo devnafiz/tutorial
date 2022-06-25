976fbca7104d8ddc78bffab1117d710ad1a2977a
devnafiz <nafiz.2011@yahoo.com>
2022-06-26 00:02:46 +0600

    Abstract Factory Design Pattern for cate gory

---
+++ class/Category.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * 
+ */
+class Category extends Abstract_Factory_Database
+{
+	//Its come to Abstract_Factory_Databasem  class for over write
+   public function connect(){
+
+   	//procedure
+   }
+
+   public function query(){
+
+
+   }
+   public function insertId()
+
+}
+
+
+
+?>
